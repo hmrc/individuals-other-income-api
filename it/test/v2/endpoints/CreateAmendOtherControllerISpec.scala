1110c2743c006710e2fde0aec6f80f623677c176
matthew-goh <186605436+matthew-goh@users.noreply.github.com>
2026-02-26 14:08:09 +0000

    Update build.sbt and AppDependencies; use it/test

--- it/test/v2/endpoints/CreateAmendOtherControllerISpec.scala
+++ it/test/v2/endpoints/CreateAmendOtherControllerISpec.scala
@@ -18,12 +18,12 @@ package v2.endpoints
 
 import common.{RuleOutsideAmendmentWindowError, RuleUnalignedCessationTaxYear}
 import play.api.http.HeaderNames.ACCEPT
-import play.api.http.Status._
+import play.api.http.Status.*
 import play.api.libs.json.{JsValue, Json}
-import play.api.libs.ws.{WSRequest, WSResponse}
+import play.api.libs.ws.{WSRequest, WSResponse, writeableOf_JsValue}
 import play.api.test.Helpers.AUTHORIZATION
 import shared.models.errors
-import shared.models.errors._
+import shared.models.errors.*
 import shared.services.{AuditStub, AuthStub, DownstreamStub, MtdIdLookupStub}
 import shared.support.IntegrationBaseSpec
 import v2.fixtures.other.CreateAmendOtherFixtures.{requestBodyJsonWithoutForeignTaxCreditRelief, requestBodyWithPCRJson}
